cb72b12d7d9fd891374fab039487879712a67056
yutingli11 <yutingli0111@gmail.com>
2020-04-13 15:38:27 +0800

    add all pages

--- app/routes/router.js
+++ app/routes/router.js
@@ -23,6 +23,60 @@ module.exports = function(app) {
         });
     });
 
+    app.get('/gallery', function(req, res) {
+        
+        res.render('gallery', {
+            title : 'Gallery'
+        });
+    });
+
+    app.get('/membership', function(req, res) {
+        
+        res.render('membership', {
+            title : 'Membership'
+        });
+    });
+
+    app.get('/news', function(req, res) {
+        
+        res.render('news', {
+            title : 'News'
+        });
+    });
+
+    app.get('/contact', function(req, res) {
+        
+        res.render('contact', {
+            title : 'Contact'
+        });
+    });
+
+    app.post('/contact', function(req, res){
+        // look up the user's account via their email //
+            // AM.getAccountByEmail(req.body['email'], function(o){
+                var o = req.body['email'];
+                if (o)
+             {
+                //     EM.dispatchResetPasswordLink(o, function(e, m){
+                //     // this callback takes a moment to return //
+                //     // TODO add an ajax loader to give user feedback //
+                //         if (!e)
+                //    {
+                //             res.status(200).send('ok');
+                //         }
+                //    else
+                //    {
+                //             for (k in e) console.log('ERROR : ', k, e[k]);
+                //             res.status(400).send('unable to dispatch password reset');
+                //         }
+                //     });
+                }
+             else
+             {
+                    res.status(400).send('email-not-found');
+                }
+            // });
+        });
 
 
     app.get('/about', function(req, res) {
