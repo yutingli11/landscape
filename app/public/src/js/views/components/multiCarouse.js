f755ae881cbb9d21fe8229dd5664a1912f4649ef
yutingli11 <yutingli0111@gmail.com>
2020-04-17 17:33:42 +0800

    day10

--- app/public/src/js/views/components/multiCarouse.js
+++ app/public/src/js/views/components/multiCarouse.js
@@ -10,17 +10,17 @@ $(document).ready(function () {
         var $e = $(e.relatedTarget);
         var idx = $e.index();
         var itemsPerSlide = 4;
-        var totalItems = $('.carousel-item').length;
+        var totalItems = $('.multiCarouselItem').length;
 
         if (idx >= totalItems - (itemsPerSlide - 1)) {
             var it = itemsPerSlide - (totalItems - idx);
             for (var i = 0; i < it; i++) {
                 // append slides to end
                 if (e.direction == "left") {
-                    $('.carousel-item').eq(i).appendTo('.carousel-inner');
+                    $('.multiCarouselItem').eq(i).appendTo('.multiCarouselItemInner');
                 }
                 else {
-                    $('.carousel-item').eq(0).appendTo('.carousel-inner');
+                    $('.multiCarouselItem').eq(0).appendTo('.multiCarouselItemInner');
                 }
             }
         }
