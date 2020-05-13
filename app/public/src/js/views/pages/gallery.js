a0f46ba46710767313972f433dee3a7d4c2136d1
yutingli11 <yutingli0111@gmail.com>
2020-05-13 18:48:16 +0800

    add en txt

--- app/public/src/js/views/pages/gallery.js
+++ app/public/src/js/views/pages/gallery.js
@@ -2,34 +2,46 @@
 $(document).ready(function ($) {
     var galleryObjs = [
         {
-            p: [{ img: '/img/gallery/1.jpg', txtHeader: '添加文字1', txtContent: '添加文字1' },
-            { img: '/img/gallery/2.jpg', txtHeader: '添加文字2', txtContent: '添加文字2' },
-            { img: '/img/gallery/3.jpg', txtHeader: '添加文字3', txtContent: '添加文字3' }]
+            p: [
+                { img: '/img/gallery/1.jpg', txtHeader: 'one', txtContent: '' },
+                { img: '/img/gallery/2.jpg', txtHeader: 'two', txtContent: '' },
+                { img: '/img/gallery/3.jpg', txtHeader: 'three', txtContent: '' }
+            ]
         },
         {
-            p: [{ img: '/img/gallery/4.jpg', txtHeader: '添加文字4', txtContent: '添加文字4' },
-            { img: '/img/gallery/5.jpg', txtHeader: '添加文字5', txtContent: '添加文字5' },
-            { img: '/img/gallery/6.jpg', txtHeader: '添加文字6', txtContent: '添加文字6' }]
+            p: [
+                { img: '/img/gallery/4.jpg', txtHeader: 'four', txtContent: '' },
+                { img: '/img/gallery/5.jpg', txtHeader: 'five', txtContent: '' },
+                { img: '/img/gallery/6.jpg', txtHeader: 'six', txtContent: '' }
+            ]
         },
         {
-            p: [{ img: '/img/gallery/7.jpg', txtHeader: '添加文字7', txtContent: '添加文字7' },
-            { img: '/img/gallery/8.jpg', txtHeader: '添加文字8', txtContent: '添加文字8' },
-            { img: '/img/gallery/9.jpg', txtHeader: '添加文字9', txtContent: '添加文字9' }]
+            p: [
+                { img: '/img/gallery/7.jpg', txtHeader: 'seven', txtContent: '' },
+                { img: '/img/gallery/8.jpg', txtHeader: 'eight', txtContent: '' },
+                { img: '/img/gallery/9.jpg', txtHeader: 'nine', txtContent: '' }
+            ]
         },
         {
-            p: [{ img: '/img/gallery/10.jpg', txtHeader: '添加文字1', txtContent: '添加文字1' },
-            { img: '/img/gallery/11.jpg', txtHeader: '添加文字2', txtContent: '添加文字2' },
-            { img: '/img/gallery/12.jpg', txtHeader: '添加文字3', txtContent: '添加文字3' }]
+            p: [
+                { img: '/img/gallery/10.jpg', txtHeader: 'ten', txtContent: '' },
+                { img: '/img/gallery/11.jpg', txtHeader: 'eleven', txtContent: '' },
+                { img: '/img/gallery/12.jpg', txtHeader: 'twelve', txtContent: '' }
+            ]
         },
         {
-            p: [{ img: '/img/gallery/13.jpg', txtHeader: '添加文字4', txtContent: '添加文字4' },
-            { img: '/img/gallery/14.jpg', txtHeader: '添加文字5', txtContent: '添加文字5' },
-            { img: '/img/gallery/15.jpg', txtHeader: '添加文字6', txtContent: '添加文字6' }]
+            p: [
+                { img: '/img/gallery/13.jpg', txtHeader: 'thirteen', txtContent: '' },
+                { img: '/img/gallery/14.jpg', txtHeader: 'fourteen', txtContent: '' },
+                { img: '/img/gallery/15.jpg', txtHeader: 'fifteen', txtContent: '' }
+            ]
         },
         {
-            p: [{ img: '/img/gallery/16.jpg', txtHeader: '添加文字7', txtContent: '添加文字7' },
-            { img: '/img/gallery/17.jpg', txtHeader: '添加文字8', txtContent: '添加文字8' },
-            { img: '/img/gallery/18.jpg', txtHeader: '添加文字9', txtContent: '添加文字9' }]
+            p: [
+                { img: '/img/gallery/16.jpg', txtHeader: 'sixteen', txtContent: '' },
+                { img: '/img/gallery/17.jpg', txtHeader: 'seventeen', txtContent: '' },
+                { img: '/img/gallery/18.jpg', txtHeader: 'eighteen', txtContent: '' }
+            ]
         }
     ];
     var template = document.getElementById('galleryHolder-template').innerHTML;
--- app/public/src/js/views/pages/projects.js
+++ app/public/src/js/views/pages/projects.js
@@ -2,19 +2,25 @@
 $(document).ready(function ($) {
     var houseLayoutObjs = [
         {
-            p: [{ img: '/img/projects/houseLayout/houseLayout1.jpg', txtHeader: '添加文字1', txtContent: '添加文字1' },
-            { img: '/img/projects/houseLayout/houseLayout2.jpg', txtHeader: '添加文字2', txtContent: '添加文字2' },
-            { img: '/img/projects/houseLayout/houseLayout3.jpg', txtHeader: '添加文字3', txtContent: '添加文字3' }]
+            p: [
+                { img: '/img/projects/houseLayout/houseLayout1.jpg', txtHeader: 'one', txtContent: '' },
+                { img: '/img/projects/houseLayout/houseLayout2.jpg', txtHeader: 'two', txtContent: '' },
+                { img: '/img/projects/houseLayout/houseLayout3.jpg', txtHeader: 'three', txtContent: '' }
+            ]
         },
         {
-            p: [{ img: '/img/projects/houseLayout/houseLayout4.jpg', txtHeader: '添加文字4', txtContent: '添加文字4' },
-            { img: '/img/projects/houseLayout/houseLayout5.jpg', txtHeader: '添加文字5', txtContent: '添加文字5' },
-            { img: '/img/projects/houseLayout/houseLayout6.jpg', txtHeader: '添加文字6', txtContent: '添加文字6' }]
+            p: [
+                { img: '/img/projects/houseLayout/houseLayout4.jpg', txtHeader: 'four', txtContent: '' },
+                { img: '/img/projects/houseLayout/houseLayout5.jpg', txtHeader: 'five', txtContent: '' },
+                { img: '/img/projects/houseLayout/houseLayout6.jpg', txtHeader: 'six', txtContent: '' }
+            ]
         },
         {
-            p: [{ img: '/img/projects/houseLayout/houseLayout7.jpg', txtHeader: '添加文字7', txtContent: '添加文字7' },
-            { img: '/img/projects/houseLayout/houseLayout8.jpg', txtHeader: '添加文字8', txtContent: '添加文字8' },
-            { img: '/img/projects/houseLayout/houseLayout9.jpg', txtHeader: '添加文字9', txtContent: '添加文字9' }]
+            p: [
+                { img: '/img/projects/houseLayout/houseLayout7.jpg', txtHeader: 'seven', txtContent: '' },
+                { img: '/img/projects/houseLayout/houseLayout8.jpg', txtHeader: 'eight', txtContent: '' },
+                { img: '/img/projects/houseLayout/houseLayout9.jpg', txtHeader: 'nine', txtContent: '' }
+            ]
         }
     ];
     var template = document.getElementById('houseLayoutHolder-template').innerHTML;
@@ -28,19 +34,25 @@ $(document).ready(function ($) {
 
     var projectDesignObjs = [
         {
-            p: [{ img: '/img/projects/gardenDesignRef/1.jpg', txtHeader: '添加文字1', txtContent: '添加文字1' },
-            { img: '/img/projects/gardenDesignRef/2.jpg', txtHeader: '添加文字2', txtContent: '添加文字2' },
-            { img: '/img/projects/gardenDesignRef/3.jpg', txtHeader: '添加文字3', txtContent: '添加文字3' }]
+            p: [
+                { img: '/img/projects/gardenDesignRef/1.jpg', txtHeader: 'one', txtContent: '' },
+                { img: '/img/projects/gardenDesignRef/2.jpg', txtHeader: 'two', txtContent: '' },
+                { img: '/img/projects/gardenDesignRef/3.jpg', txtHeader: 'three', txtContent: '' }
+            ]
         },
         {
-            p: [{ img: '/img/projects/gardenDesignRef/4.jpg', txtHeader: '添加文字4', txtContent: '添加文字4' },
-            { img: '/img/projects/gardenDesignRef/5.jpg', txtHeader: '添加文字5', txtContent: '添加文字5' },
-            { img: '/img/projects/gardenDesignRef/6.jpg', txtHeader: '添加文字6', txtContent: '添加文字6' }]
+            p: [
+                { img: '/img/projects/gardenDesignRef/4.jpg', txtHeader: 'four', txtContent: '' },
+                { img: '/img/projects/gardenDesignRef/5.jpg', txtHeader: 'five', txtContent: '' },
+                { img: '/img/projects/gardenDesignRef/6.jpg', txtHeader: 'six', txtContent: '' }
+            ]
         },
         {
-            p: [{ img: '/img/projects/gardenDesignRef/7.jpg', txtHeader: '添加文字7', txtContent: '添加文字7' },
-            { img: '/img/projects/gardenDesignRef/8.jpg', txtHeader: '添加文字8', txtContent: '添加文字8' },
-            { img: '/img/projects/gardenDesignRef/9.jpg', txtHeader: '添加文字9', txtContent: '添加文字9' }]
+            p: [
+                { img: '/img/projects/gardenDesignRef/7.jpg', txtHeader: 'seven', txtContent: '' },
+                { img: '/img/projects/gardenDesignRef/8.jpg', txtHeader: 'eight', txtContent: '' },
+                { img: '/img/projects/gardenDesignRef/9.jpg', txtHeader: 'nine', txtContent: '' }
+            ]
         }
     ];
     var template = document.getElementById('projectDesignHolder-template').innerHTML;
@@ -54,54 +66,74 @@ $(document).ready(function ($) {
 
     var amenitiesObjs = [
         {
-            p: [{ img: '/img/projects/amenities/food/1.jpg', txtHeader: '添加文字1', txtContent: '添加文字1' },
-            { img: '/img/projects/amenities/food/2.jpg', txtHeader: '添加文字2', txtContent: '添加文字2' },
-            { img: '/img/projects/amenities/food/3.jpg', txtHeader: '添加文字3', txtContent: '添加文字3' }]
+            p: [
+                { img: '/img/projects/amenities/food/1.jpg', txtHeader: 'French restaurant', txtContent: '' },
+                { img: '/img/projects/amenities/food/2.jpg', txtHeader: 'Thai restaurant', txtContent: '' },
+                { img: '/img/projects/amenities/food/3.jpg', txtHeader: 'Cambodian restaurant', txtContent: '' }
+            ]
         },
         {
-            p: [{ img: '/img/projects/amenities/food/4.jpg', txtHeader: '添加文字4', txtContent: '添加文字4' },
-            { img: '/img/projects/amenities/food/5.jpg', txtHeader: '添加文字5', txtContent: '添加文字5' },
-            { img: '/img/projects/amenities/food/6.jpg', txtHeader: '添加文字6', txtContent: '添加文字6' }]
+            p: [
+                { img: '/img/projects/amenities/food/4.jpg', txtHeader: 'Chinese hot pot', txtContent: '' },
+                { img: '/img/projects/amenities/food/5.jpg', txtHeader: 'Vietnamese restaurant', txtContent: '' },
+                { img: '/img/projects/amenities/food/6.jpg', txtHeader: 'Japanese restaurant', txtContent: '' }
+            ]
         },
         {
-            p: [{ img: '/img/projects/amenities/fun/1.jpg', txtHeader: '添加文字7', txtContent: '添加文字7' },
-            { img: '/img/projects/amenities/fun/2.jpg', txtHeader: '添加文字8', txtContent: '添加文字8' },
-            { img: '/img/projects/amenities/fun/3.jpg', txtHeader: '添加文字9', txtContent: '添加文字9' }]
+            p: [
+                { img: '/img/projects/amenities/fun/1.jpg', txtHeader: 'Gaming', txtContent: '' },
+                { img: '/img/projects/amenities/fun/2.jpg', txtHeader: 'Beach volleyball', txtContent: '' },
+                { img: '/img/projects/amenities/fun/3.jpg', txtHeader: 'Performance show', txtContent: '' }
+            ]
         },
         {
-            p: [{ img: '/img/projects/amenities/fun/4.jpg', txtHeader: '添加文字7', txtContent: '添加文字7' },
-            { img: '/img/projects/amenities/fun/5.jpg', txtHeader: '添加文字8', txtContent: '添加文字8' },
-            { img: '/img/projects/amenities/fun/6.jpg', txtHeader: '添加文字9', txtContent: '添加文字9' }]
+            p: [
+                { img: '/img/projects/amenities/fun/4.jpg', txtHeader: 'Open-air cinema', txtContent: '' },
+                { img: '/img/projects/amenities/fun/5.jpg', txtHeader: 'Archery', txtContent: '' },
+                { img: '/img/projects/amenities/fun/6.jpg', txtHeader: 'Bar street', txtContent: '' }
+            ]
         },
         {
-            p: [{ img: '/img/projects/amenities/outdoor/1.jpg', txtHeader: '添加文字7', txtContent: '添加文字7' },
-            { img: '/img/projects/amenities/outdoor/2.jpg', txtHeader: '添加文字8', txtContent: '添加文字8' },
-            { img: '/img/projects/amenities/outdoor/3.jpg', txtHeader: '添加文字9', txtContent: '添加文字9' }]
+            p: [
+                { img: '/img/projects/amenities/outdoor/1.jpg', txtHeader: 'Self-driving tour', txtContent: '' },
+                { img: '/img/projects/amenities/outdoor/2.jpg', txtHeader: 'Motorbike', txtContent: '' },
+                { img: '/img/projects/amenities/outdoor/3.jpg', txtHeader: 'Bike riding', txtContent: '' }
+            ]
         },
         {
-            p: [{ img: '/img/projects/amenities/outdoor/4.jpg', txtHeader: '添加文字7', txtContent: '添加文字7' },
-            { img: '/img/projects/amenities/outdoor/5.jpg', txtHeader: '添加文字8', txtContent: '添加文字8' },
-            { img: '/img/projects/amenities/outdoor/6.jpg', txtHeader: '添加文字9', txtContent: '添加文字9' }]
+            p: [
+                { img: '/img/projects/amenities/outdoor/4.jpg', txtHeader: 'Mountain climbing', txtContent: '' },
+                { img: '/img/projects/amenities/outdoor/5.jpg', txtHeader: 'Boat tour', txtContent: '' },
+                { img: '/img/projects/amenities/outdoor/6.jpg', txtHeader: 'Hiking', txtContent: '' }
+            ]
         },
         {
-            p: [{ img: '/img/projects/amenities/shopping/1.jpg', txtHeader: '添加文字7', txtContent: '添加文字7' },
-            { img: '/img/projects/amenities/shopping/2.jpg', txtHeader: '添加文字8', txtContent: '添加文字8' },
-            { img: '/img/projects/amenities/shopping/3.jpg', txtHeader: '添加文字9', txtContent: '添加文字9' }]
+            p: [
+                { img: '/img/projects/amenities/shopping/1.jpg', txtHeader: 'Kampot Durian', txtContent: '' },
+                { img: '/img/projects/amenities/shopping/2.jpg', txtHeader: 'Local Vegetables', txtContent: '' },
+                { img: '/img/projects/amenities/shopping/3.jpg', txtHeader: 'Outlet Mall', txtContent: '' }
+            ]
         },
         {
-            p: [{ img: '/img/projects/amenities/shopping/4.jpg', txtHeader: '添加文字7', txtContent: '添加文字7' },
-            { img: '/img/projects/amenities/shopping/5.jpg', txtHeader: '添加文字8', txtContent: '添加文字8' },
-            { img: '/img/projects/amenities/shopping/6.jpg', txtHeader: '添加文字9', txtContent: '添加文字9' }]
+            p: [
+                { img: '/img/projects/amenities/shopping/4.jpg', txtHeader: 'Seafood Market', txtContent: '' },
+                { img: '/img/projects/amenities/shopping/5.jpg', txtHeader: 'Cambodian Crafts', txtContent: '' },
+                { img: '/img/projects/amenities/shopping/6.jpg', txtHeader: 'Flower Market', txtContent: '' }
+            ]
         },
         {
-            p: [{ img: '/img/projects/amenities/travel/1.jpg', txtHeader: '添加文字7', txtContent: '添加文字7' },
-            { img: '/img/projects/amenities/travel/2.jpg', txtHeader: '添加文字8', txtContent: '添加文字8' },
-            { img: '/img/projects/amenities/travel/3.jpg', txtHeader: '添加文字9', txtContent: '添加文字9' }]
+            p: [
+                { img: '/img/projects/amenities/travel/1.jpg', txtHeader: 'Phu Quoc Island', txtContent: '' },
+                { img: '/img/projects/amenities/travel/2.jpg', txtHeader: 'Bokor Mountain', txtContent: '' },
+                { img: '/img/projects/amenities/travel/3.jpg', txtHeader: 'Kep city', txtContent: '' }
+            ]
         },
         {
-            p: [{ img: '/img/projects/amenities/travel/4.jpg', txtHeader: '添加文字7', txtContent: '添加文字7' },
-            { img: '/img/projects/amenities/travel/5.jpg', txtHeader: '添加文字8', txtContent: '添加文字8' },
-            { img: '/img/projects/amenities/travel/6.jpg', txtHeader: '添加文字9', txtContent: '添加文字9' }]
+            p: [
+                { img: '/img/projects/amenities/travel/4.jpg', txtHeader: 'Kampot city', txtContent: '' },
+                { img: '/img/projects/amenities/travel/5.jpg', txtHeader: 'Koh Rong Island', txtContent: '' },
+                { img: '/img/projects/amenities/travel/6.jpg', txtHeader: 'Sihanoukville', txtContent: '' }
+            ]
         }
     ];
     var template = document.getElementById('amenitiesHolder-template').innerHTML;
